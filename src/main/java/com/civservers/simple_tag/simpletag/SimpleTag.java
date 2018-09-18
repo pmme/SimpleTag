68f7c1569bcae014022267572eaf3ace4d33c685
Jonathan Elder <john.elder.h@gmail.com>
2018-09-18 13:27:22 -0700

    plugin prep
    removed un-needed template parts.  Updated plugin.yml.

--- src/main/java/com/civservers/simple_tag/simpletag/SimpleTag.java
+++ src/main/java/com/civservers/simple_tag/simpletag/SimpleTag.java
@@ -9,43 +9,18 @@ import org.bukkit.scheduler.BukkitScheduler;
 
 public final class SimpleTag extends JavaPlugin {
 
-	Object playerList;
-	public String pluginName = "BlankPlugin";
+	public String pluginName = "SimpleTag";
 	
 	@Override
     public void onEnable() {
 		
-		this.getCommand("mycmd").setExecutor(new pluginCommandExecutor(this));
-		this.getCommand("mycmd2").setExecutor(new pluginCommandExecutor(this));
-		this.getCommand("mycmd3").setExecutor(new pluginCommandExecutor(this));
-
-		BukkitScheduler scheduler = getServer().getScheduler();
-        scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
-            @Override
-            public void run() {
-                // Do something
-            	for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-            		player.sendMessage("Count");
-            	}
-            }
-        }, 0L, 20L);
-		
-		
-    	for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-    		// cycle through online players and do something.
-
-    	    /*pl is an object . put (pname , return of function run on player object )*/
-			/*playerList.put(player.getName(), playerData(player));*/
-    	}
-    	    	
-    	Bukkit.getConsoleSender().sendMessage("["+pluginName+"] Loaded");
-    	
+		this.getCommand("simpletag").setExecutor(new pluginCommandExecutor(this));
 
     }
     
     @Override
     public void onDisable() {
-    	Bukkit.getConsoleSender().sendMessage("["+pluginName+"] Unloaded");
+    	
     }
 
 }
--- src/main/java/com/civservers/simple_tag/simpletag/pluginCommandExecutor.java
+++ src/main/java/com/civservers/simple_tag/simpletag/pluginCommandExecutor.java
@@ -8,8 +8,17 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class pluginCommandExecutor implements CommandExecutor {
 	private final SimpleTag plugin;
+	private String[] helpMsg = {
+			ChatColor.RED + "" + ChatColor.BOLD + "--- SimpleTag Help ---" + ChatColor.RESET,
+			ChatColor.GREEN + "" + ChatColor.BOLD + "Please try one of these player comamnds:",
+			ChatColor.RESET + "" + ChatColor.GREEN + " simpletag start | join <player> | leave | leaveall | end | kick <player>",
+			ChatColor.YELLOW + "" + ChatColor.BOLD + "or one of these admin commands:",
+			ChatColor.RESET + "" + ChatColor.YELLOW + " /simpletag endall | reload"
+	};
 	
 	public pluginCommandExecutor(SimpleTag plugin) {
 		this.plugin = plugin;
@@ -19,60 +28,32 @@ public class pluginCommandExecutor implements CommandExecutor {
 	
 	@Override	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-/* SAMPLE COMMANDS */
-    	if (cmd.getName().equalsIgnoreCase("mycmd")) { // If the player typed /basic then do the following, note: If you only registered this executor for one command, you don't need this
-    		// doSomething
-
-    		
-    		for (World world : Bukkit.getWorlds()) {
-    			for (Entity entity : world.getEntities()) {
-        			Bukkit.getConsoleSender().sendMessage(world.getName() + ": " + entity.getType() + " > " + entity.getName());    				
-    			}
-
-    			
-    		}
-    		
-
-    		
-    		return true;   		
-    		
-    		
-    	} else if (cmd.getName().equalsIgnoreCase("mycmd2")) {
-// THIS COMMAND MUST BE RUN BY A PLAYER
-    		if (!(sender instanceof Player)) {
-    			sender.sendMessage("This command can only be run by a player.");
-    		} else {
-    			Player player = (Player) sender;
-    			sender.sendMessage("Command completed sucsessfully!");
-    		}
-    		return true;
-    		
-    		
-    		
-// THIS CMD MUST HAVE THE RIGHT NUMBER OF ARGUMENTS
-    	} else if (cmd.getName().equalsIgnoreCase("mycmd3")) {
-    		if (args.length > 1) {
-    			sender.sendMessage("Too many arguments");
-    			return false;
-    		} else if (args.length < 1) {
-    			String reMsg[] = {"Not enough arguments!","Try again..."};
+		
+		
+		
+    	if (cmd.getName().equalsIgnoreCase("simpletag")) {
+    		if (args.length < 1) {
+    			String reMsg[] = helpMsg;
     			sender.sendMessage(reMsg);
+    			return false;
+    		} else if (args[0].equalsIgnoreCase("reload")){
+    			
+//    			boolean rStatus = plugin.reload();
+//    			if (rStatus) {
+//    				sender.sendMessage(ChatColor.GREEN + plugin.pluginName + " Reloaded!");
+//    			} else {
+    				sender.sendMessage(ChatColor.RED + plugin.pluginName + " Reload Failed!");
+//    			}
+    			return true;
     		} else {
-    			Player target = (Bukkit.getServer().getPlayer(args[0]));
-    			if (target == null) {
-    				sender.sendMessage(args[0] + " is not online!");
-    				return false;
-    			} else {
-    				sender.sendMessage("We've found " + args[0]);
-    				return true;
-    			}
-    		}
+    			String reMsg[] = helpMsg;
+    			sender.sendMessage(reMsg);
+        		return false;
+    		}   		
+    		
     	} else {
-// THESE COMMANDS DO NOT EXIST
-    		sender.sendMessage("I really don't understand what you want me to do.");
     		return false;
     	}
-    	return false;
 	}
 	
 }

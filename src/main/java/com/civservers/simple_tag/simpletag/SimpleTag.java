9d08cabd7b3307813da809a617932a1d90825e8c
Jonathan Elder <john.elder.h@gmail.com>
2018-09-18 15:41:51 -0700

    added commands and config
    created start and stop commands
    added default configs
    added session saving in games config

--- src/main/java/com/civservers/simple_tag/simpletag/SimpleTag.java
+++ src/main/java/com/civservers/simple_tag/simpletag/SimpleTag.java
@@ -1,26 +1,84 @@
 package com.civservers.simple_tag.simpletag;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
 
 
-public final class SimpleTag extends JavaPlugin {
+public final class SimpleTag extends JavaPlugin implements Listener {
 
 	public String pluginName = "SimpleTag";
+	public FileConfiguration config = getConfig();
+	
 	
 	@Override
     public void onEnable() {
 		
+		config.addDefault("cancelPVPDamage", true);
+		
+		config.options().copyDefaults(true);
+	    saveConfig();
+	    
+	    
 		this.getCommand("simpletag").setExecutor(new pluginCommandExecutor(this));
+		
+		Bukkit.getPluginManager().registerEvents(this, this);
 
     }
     
     @Override
     public void onDisable() {
-    	
+		if (config.contains("games")) {
+			config.set("games", null);
+			saveConfig();
+		}
+    }
+    
+    @EventHandler
+    public void onHit(EntityDamageByEntityEvent event){    	
+    	// Check if damage is pvp
+    	if (event.getEntity() instanceof Player && event.getDamager() instanceof Player) {
+    		Player victim = (Player) event.getEntity();
+    		Player culprit = (Player) event.getDamager();
+    		/*
+    		 * if culprit is it
+    		 * 		if victim is playing
+    		 * 			it = victim
+    		 * 			lastit = cuplrit
+    		 * 			restart tagback timer
+    		 * 
+    		 * 
+    		 * 
+    		 */
+    		
+    		
+    		
+    		//Cancel damage
+    		if ((boolean) config.get("cancelPVPDamage")) {
+    			event.setCancelled(true);
+    		}
+    	}
+    }
+    
+    public boolean isPlaying(String uuid) {
+    	boolean out = false;
+    	if (config.contains("games")) {
+	    	for (Object key : config.getConfigurationSection("games").getKeys(false).toArray()) {
+	    		for (Object playing : config.getConfigurationSection("games." + key + ".players").getKeys(false).toArray()) {
+	    			if (playing.equals(uuid)) {
+	    				out = true;
+	    			}
+	    		}
+	    	}
+    	}
+		return out;
     }
 
 }
--- src/main/java/com/civservers/simple_tag/simpletag/pluginCommandExecutor.java
+++ src/main/java/com/civservers/simple_tag/simpletag/pluginCommandExecutor.java
@@ -1,10 +1,14 @@
 package com.civservers.simple_tag.simpletag;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
@@ -12,13 +16,20 @@ import net.md_5.bungee.api.ChatColor;
 
 public class pluginCommandExecutor implements CommandExecutor {
 	private final SimpleTag plugin;
+	
 	private String[] helpMsg = {
 			ChatColor.RED + "" + ChatColor.BOLD + "--- SimpleTag Help ---" + ChatColor.RESET,
 			ChatColor.GREEN + "" + ChatColor.BOLD + "Please try one of these player comamnds:",
-			ChatColor.RESET + "" + ChatColor.GREEN + " simpletag start | join <player> | leave | leaveall | end | kick <player>",
+			ChatColor.RESET + "" + ChatColor.GREEN + " simpletag start | join <player> | leave | leaveall | stop | kick <player>",
 			ChatColor.YELLOW + "" + ChatColor.BOLD + "or one of these admin commands:",
 			ChatColor.RESET + "" + ChatColor.YELLOW + " /simpletag endall | reload"
 	};
+	
+	private String[] noConsole = {ChatColor.RED + "Sorry, you must be a player to run this command."};
+	
+	private String[] alreadyPlaying = {ChatColor.RED + "You are already playing a gam of tag!"};
+	
+	
 	
 	public pluginCommandExecutor(SimpleTag plugin) {
 		this.plugin = plugin;
@@ -28,11 +39,44 @@ public class pluginCommandExecutor implements CommandExecutor {
 	
 	@Override	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-
+		
     	if (cmd.getName().equalsIgnoreCase("simpletag")) {
     		if (args.length < 1) {
     			sender.sendMessage(helpMsg);
     			return false;
+    		} else if (args[0].equalsIgnoreCase("start")){
+    			if (sender instanceof Player) {
+    				Player player = (Player) sender;
+    				String uuid = player.getUniqueId().toString();
+    				if (plugin.config.contains("games." + uuid) || plugin.isPlaying(uuid)) {
+    					// player is already playing a game of tag.  Cannot create a new one.
+    					sender.sendMessage(alreadyPlaying);
+    					return false;
+    				} else {
+    					List<String> playerList = new ArrayList<String>();
+    					playerList.add(uuid);
+    					// create game and player list
+    					plugin.config.set("games." + uuid + ".players", playerList);
+    					// set initial "it"
+    					plugin.config.set("games." + uuid + ".it", uuid);
+    					plugin.saveConfig();
+    				}
+    				
+    			} else {
+    				sender.sendMessage(noConsole);
+    				return false;
+    			}
+    		} else if (args[0].equalsIgnoreCase("stop")){
+    			if (sender instanceof Player) {
+    				Player player = (Player) sender;
+    				String uuid = player.getUniqueId().toString();
+	    			if (plugin.config.contains("games." + uuid)) {
+	    				//TODO: Send to each player in game
+	    				plugin.config.set("games." + uuid, null);
+	    				plugin.saveConfig();
+	    			}
+    			}
+
     		} else if (args[0].equalsIgnoreCase("reload")){
     			
 //    			boolean rStatus = plugin.reload();
@@ -50,6 +94,7 @@ public class pluginCommandExecutor implements CommandExecutor {
     	} else {
     		return false;
     	}
+		return false;
 	}
 	
 }
